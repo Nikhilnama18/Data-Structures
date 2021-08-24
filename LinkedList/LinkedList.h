0ea2daf88fa8381ec633a2ecfa2b9c4b916904f7
Nikhilnama18 <nikhilnama1111@gmail.com>
2021-08-24 10:52:40 +0530

    Completed Linked List

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -8,24 +8,17 @@ class LinkedList
 {
 private:
     int len;
-
-public:
     Node<T> *head;
     Node<T> *tail;
+
+public:
     LinkedList();
-    // linkedlist(T x);
     void push_back(T data);
-    void insert(int pos);
+    void insert(T data, int pos);
     void pop_back();
     void remove(int pos);
     void reverse();
     void print();
     int size();
 };
-// template <typename T>
-// LinkedList<T>::LinkedList() : len(0), head(nullptr) {}
-
-// template <typename T>
-// linkedlist<T>::linkedlist(T x) : size(0), val(0), next(nullptr) {}
-
 #endif // linkelist.h
